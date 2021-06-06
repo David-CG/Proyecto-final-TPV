f0da243d355dc848e5229ab91c767fd341c1ec3b
dacug <dacugam2000@gmail.com>
2021-06-07 01:13:35 +0200

    Avances en el panel.

--- ProyectoFinal/src/TPV.java
+++ ProyectoFinal/src/TPV.java
@@ -1,19 +1,24 @@
 import javax.swing.*;
-import java.awt.GridLayout;
+import java.awt.*;
 
 public class TPV {
 
     JFrame frame;
-    JPanel panel;
-    JButton boton;
+    JPanel panelizq;
+    JPanel panelder;
+    JPanel mi;
+    JPanel redmi;
+    JPanel poco;
     ImageIcon logo;
 
     void ventana() {
         frame = new JFrame("Xiaomi Elche");
         frame.setLayout(new GridLayout());
-        frame.add(panel);
+        frame.add(panelizq);
+        frame.add(panelder);
         frame.pack();
-        frame.setSize(800, 500);
+        frame.setSize(1280, 720);
+        frame.setLocation(100,50);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
@@ -21,15 +26,29 @@ public class TPV {
         frame.setIconImage(logo.getImage());
     }
 
-    void panelPrincipal() {
-        panel = new JPanel();
-        panel.setLayout(new GridLayout(0, 5, 5, 5));
-        boton = new JButton("");
+    void panelIzq() {
+        panelizq = new JPanel();
+        mi = new JPanel();
+        redmi = new JPanel();
+        poco = new JPanel();
+        panelizq.setLayout(new GridLayout(0, 1, 5, 5));
+        panelizq.setBorder(BorderFactory.createMatteBorder(5, 5, 5, 0, Color.getHSBColor(0, 100, 256)));
+        JTabbedPane tp = new JTabbedPane();
+        tp.add("Mi", mi);
+        tp.add("RedMi", redmi);
+        tp.add("POCO", poco);
+        panelizq.add(tp);
+    }
 
+    void panelDer() {
+        panelder = new JPanel();
+        panelder.setLayout(new GridLayout());
+        panelder.setBorder(BorderFactory.createMatteBorder(5, 5, 5, 5, Color.getHSBColor(0, 100, 256)));
     }
 
     public TPV() {
-        panelPrincipal();
+        panelIzq();
+        panelDer();
         ventana();
     }
 
